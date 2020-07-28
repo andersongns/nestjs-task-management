098d7fe8fe9632d3637ff96a43d85abd0f59ecf7
Anderson Santos <anderson.a.santos@accenture.com>
2020-07-28 20:19:42 -0300

    finish section 3

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,22 @@
+import { PipeTransform, BadRequestException } from "@nestjs/common";
+import { TaskStatus } from "../task.model";
+
+export class TaskStatusValidationPipe implements PipeTransform {
+
+    readonly allowStatuses = [
+        TaskStatus.OPEN,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.DONE,
+    ]
+
+    private isStatusValid(status: any): boolean {
+        return this.allowStatuses.indexOf(status) !== -1;
+    }
+
+    transform(value: string): string {
+        value = value.toUpperCase();
+        if(!this.isStatusValid(value)) { throw new BadRequestException(`${value} is an invalid status`) }
+        return value;
+    }
+
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -29,7 +29,13 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find(task => task.id === id);
+        const found = this.tasks.find(task => task.id === id);
+
+        if(!found){
+            throw new NotFoundException(`Task with Id ${id} not found`);
+        }
+
+        return found;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -46,12 +52,13 @@ export class TasksService {
     }
 
     updateTaskStatus(id: string, status: TaskStatus): Task {
-        const task = this.tasks.find(task => task.id === id);
+        const task = this.getTaskById(id);
         task.status = status;
         return task;
     }
 
     deleteTask(id: string): void {
-        this.tasks = this.tasks.filter( task => task.id !== id);
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter( task => task.id !== found.id);
     }
 }

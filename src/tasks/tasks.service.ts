5de52bf3692face4f4e2b3a91720288a56d6d7e6
Anderson Santos <anderson.a.santos@accenture.com>
2020-07-28 18:41:19 -0300

    finish section 2

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v1 } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = [];
+
+    getTasks(): Task[] {
+        return this.tasks;
+    }
+
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+
+        const { status, search } = filterDto;
+        let tasks = this.getTasks();
+        
+        if (status) {
+            tasks = tasks.filter(task => task.status === status);
+        }
+        
+        if (search) {
+            tasks = tasks.filter(task => task.title.includes(search) || task.description.includes(search));
+        }
+
+        return tasks;
+    }
+
+    getTaskById(id: string): Task {
+        return this.tasks.find(task => task.id === id);
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const { title, description } = createTaskDto;
+        const task: Task = {
+            id: v1(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        };
+
+        this.tasks.push(task);
+        return task;
+    }
+
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+        const task = this.tasks.find(task => task.id === id);
+        task.status = status;
+        return task;
+    }
+
+    deleteTask(id: string): void {
+        this.tasks = this.tasks.filter( task => task.id !== id);
+    }
+}
